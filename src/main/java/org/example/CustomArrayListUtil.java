36e20070391aef0a15ed8d0de729fb60682259fa
dariakoval <dasharibkavmore666@mail.ru>
2024-01-27 12:16:45 +0400

    refactor CustomArrayListUtil.java

--- src/main/java/org/example/CustomArrayListUtil.java
+++ src/main/java/org/example/CustomArrayListUtil.java
@@ -31,31 +31,38 @@ public class CustomArrayListUtil {
     }
 
     private static <T> CustomArrayList<T> quickSort(CustomArrayList<T> list, Comparator<T> comparator) {
-        if (list.getSize() < 2) {
-            return list;
+        var listSize = list.getSize();
+
+        if (listSize == 0) {
+            return new CustomArrayList<>();
         }
 
-        var pivot = list.get(0);
-        var less = new CustomArrayList<T>();
-        for (int i = 1; i < list.getSize(); i++) {
-            var current = list.get(i);
-            if (comparator.compare(current, pivot) < 0) {
-                less.add(current);
+        var index = listSize / 2;
+        var pivot = list.get(index);
+
+        var smallerElements = new CustomArrayList<T>();
+        var biggerElements = new CustomArrayList<T>();
+
+        for (int i = 0; i < listSize; i++) {
+            if (i == index) {
+                continue;
+            }
+
+            var currentElement = list.get(i);
+
+            if (comparator.compare(currentElement, pivot) < 0) {
+                smallerElements.add(currentElement);
+            } else {
+                biggerElements.add(currentElement);
             }
         }
-        var greater = new CustomArrayList<T>();
-        for (int i = 1; i < list.getSize(); i++) {
-            var current = list.get(i);
-            if (comparator.compare(current, pivot) >= 0) {
-                greater.add(current);
-            }
-        }
 
-        var sortedLess = quickSort(less, comparator);
-        var sortedGreater = quickSort(greater, comparator);
+        var sortedSmallerElements = quickSort(smallerElements, comparator);
+        var sortedBiggerElements = quickSort(biggerElements, comparator);
 
-        sortedLess.add(pivot);
-        sortedLess.addAll(sortedGreater);
-        return sortedLess;
+        var result = sortedSmallerElements;
+        result.add(pivot);
+        result.addAll(sortedBiggerElements);
+        return result;
     }
 }
